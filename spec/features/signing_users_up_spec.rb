451534516dac86041bba77c638525b8eb28babdb
kabdulka <kabdulka@sfu.ca>
2017-02-11 21:46:48 -0800

    Set up user signup feature and tested it used rspec and capabyra

---
+++ spec/features/signing_users_up_spec.rb
@@ -0,0 +1,27 @@
+
+require "rails_helper"
+
+RSpec.feature "Users signup" do
+
+	scenario "with valid credentials" do
+		visit "/"
+
+		click_link "Sign up"
+		fill_in "Email", with: "user@example.com"
+		fill_in "Password", with: "password"
+		fill_in "Password confirmation", with: "password"
+		click_button "Sign up"
+
+		# The expectation is to have a sign in successfully message
+ 		# then email address
+		expect(page).to have_content("You have signed up successfully.")
+
+	end
+
+end
+
+
+
+
+
+
